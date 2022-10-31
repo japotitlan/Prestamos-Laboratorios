ac6be4a5f6ed639108aa0b6b7414e92e509bec20
Japo <hapoweiser@gmail.com>
2022-10-31 13:33:29 -0700

    estilaso pal prestamosmain

--- prestamomain.php
+++ prestamomain.php
@@ -4,28 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
     <title>Document</title>
 </head>
 <body>
-    <header>TECNM PRESTAMO DE HERRAMIENTA/EQUIPO</header>
-    <h1>PRESTAMOS</h1>
-    <H2>¿A quién le prestraras herramineta?</H2>
-    <div>
-        <form action="prestamosM2.php">
-            <button type="submit">Maestros</button>
-        </form>
-    </div>
+    <header>
+        TECNM PRESTAMO DE HERRAMIENTA/EQUIPO
+        <h1>PRESTAMOS</h1>
+    </header>
 
-    <div>
-        <form action="prestamosAl.php">
-            <button type="submit">Alumnos</button>
+    <main class="contenedor">
+        <h2>¿A Quién Se Le Prestrara Herramineta?</h2>
+        <form class="unidad" action="prestamosM2.php">
+            <button class="contenedorB" type="submit">Maestros</button>
         </form>
-    </div>
 
-    <div>
-        <form action="prestamosP.php">
-            <button type="submit">Personal</button>
+        <form class="unidad" action="prestamosAl.php">
+            <button class="contenedorB" type="submit">Alumnos</button>
         </form>
-    </div>
+
+        <form class="unidad" action="prestamosP.php">
+            <button class="contenedorB" type="submit">Personal</button>
+        </form>
+    </main>
 </body>
 </html>
