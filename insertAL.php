a16e265b4a3a7ae54892d03cdccd7460c4946a0a
AbdielLuna <abdiel_18luna@outlook.com>
2022-10-31 18:25:18 -0700

    Funcion de insertar,Imprimir_LUNA

--- insertAL.php
+++ insertAL.php
@@ -16,7 +16,7 @@ $fchE=$_POST['fechaEntrega'];
 
 
 $sql1="INSERT INTO `solicitantes_alumnos`(`id_prestamo`, `lab`, `id_herramienta`, `nombreHerramienta`, `cantidad`, `matricula`, `nombreAlumno`, `semestre`, `carrera`, `celular`, `fechaEntrega`) 
-VALUES ('$idP','$lab','$id_H','$nomH','$cantidad','$mat','$nmA','$sem','$carr','$cel','$fchE')";
+VALUES ('$idP','$lab','$idH','$nomH','$cantidad','$mat','$nmA','$sem','$carr','$cel','$fchE')";
 $query1= mysqli_query($con,$sql1);
 
 if($query1){
