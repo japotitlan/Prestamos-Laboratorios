cf3f48de56bb8f8fceded3734e168e1e254777e2
Fernando Reyes <hapoweiser@gmail.com>
2022-11-05 12:37:40 -0700

    mejor estructura para index.php

--- index.php
+++ index.php
@@ -11,18 +11,20 @@
     <header>
         <h1>Sistema De Prestamos De Herramienta ITSPP</h1>
     </header>
-
-    <form class="contenedor" action="controlador_login.php" method="post">
-        <img src="logos/logo-tecnm.png" alt="Logo Tecnm">
-        <div class="unidad">
-            <label>Usuario</label><br>
-            <input type="text" name="usuario" required>
-        </div>
-        <div class="unidad">
-            <label>Contraseña</label><br>
-            <input type="password" name="contraseña" required>
-        </div>
-        <input class="contenedorB" type="submit" name="btningresar" value="Iniciar Sesión">
-    </form>
+    
+    <main class="contenedor">
+        <form action="controlador_login.php" method="post">
+            <img src="logos/logo-tecnm.png" alt="Logo Tecnm">
+            <div class="unidad">
+                <label>Usuario</label><br>
+                <input type="text" name="usuario" required>
+            </div>
+            <div class="unidad">
+                <label>Contraseña</label><br>
+                <input type="password" name="contraseña" required>
+            </div>
+            <input class="boton" type="submit" name="btningresar" value="Iniciar Sesión">
+        </form>
+    </main>
 </body>
 </html>
