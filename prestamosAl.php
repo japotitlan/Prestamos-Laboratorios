30fa86f529519db9e5debd73aca05374b75f495c
Fernando Reyes <hapoweiser@gmail.com>
2022-11-05 15:58:57 -0700

    Estandarizacion de estilos para prestamosAL.php y titulos para el presamosM2.php

--- prestamosAl.php
+++ prestamosAl.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+    session_start();
     include("conexionDB.php");
     $con=conectar();
     $nombreUsuario= $_SESSION['usuario'];
@@ -7,104 +7,87 @@ session_start();
     $consulta = mysqli_fetch_array($query);
     $lab = $consulta['laboratorio'];
     
-  $sql="SELECT *  FROM inventario where lab = '$lab'";
-  $query=mysqli_query($con,$sql);
-
+    $sql="SELECT *  FROM inventario where lab = '$lab'";
+    $query=mysqli_query($con,$sql);
 ?>
 
-
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/style.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <title>Inventario</title>
-  </head>
-  <body>
-    <div class="container at-5">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Prestamos Alumnos</title>
+</head>
+<body>
+    <header>
+        <h1>PRESTAMOS ALUMNOS</h1>
+    </header>
+
+    <main class="container at-5">
         <div class="row">
-           <div class="col-md-3">
-            <h1>Ingresar datos</h1>
+            <div class="col-md-3">
+                <h1>Ingresar datos</h1>
             
-        <form action="insertarAL.php" method="POST">
-        <input type="number" class="form-control mb-3" name="id_prestamo" placeholder="Id del prestamo">
-        <input type="number" class="form-control mb-3" name="id_herramienta" placeholder="ID Herramienta">
-        <input type="text" class="form-control mb-3" name="nombreHerramienta" placeholder="Nombre Herramienta">
-        <input type="number" class="form-control mb-3" name="cantidad" placeholder="cantidad">
-        <input type="number" class="form-control mb-3" name="matricula" placeholder="matricula">
-        <input type="text" class="form-control mb-3" name="nombreAlumno" placeholder="nombreAlumno">
-        <input type="text" class="form-control mb-3" name="semestre" placeholder="semestre">
-        
-        <label>Selecciona una carrera</label>
-        <select name="carrera">
-
-        <option>ISC</option>
-
-        <option>Civil</option>
-
-        <option>Industrial</option>
-
-        <option>Administración</option>
-
-        </select>
-        <br><br>
-        
-        <input type="text" class="form-control mb-3" name="celular" placeholder="celular">
-        
-        <label>FECHA DE PRESTAMO</label>
-        
-        <input type="date" class="form-control mb-3" name="fechaEntrega"> 
-                                    
-            <input type="submit" class="btn btn-primary">
-            </form>
-
-           </div>
-           <div class="col-md-8">
-                            <table class="table" >
-                                <thead class="table-success table-striped" >
-                                    <tr>
-                                    <th>Clave</th>
-                                        <th>nombreHerramienta</th>
-                                        <th>descripcion</th>
-                                        <th>laboratorio</th>
-                                        <th>cantidad</th>
-                                        <th>Prestados</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                while($row=mysqli_fetch_array($query)){     
-                                
-                                ?>
-                                <tr>
-                                    <th><?php echo $row['clave']?></th>
-                                    <th><?php echo $row['nombreHerramienta']?></th>
-                                    <th><?php echo $row['descripcion']?></th>
-                                    <th><?php echo $row['lab']?></th>
-                                    <th><?php echo $row['cantidad']?></th>
-                                    <th><?php echo $row['estado']?></th>
-                                    
-                                </tr>
-                                <?php
-                                }
-                                ?>
-                                </tbody>
-
-                            </table>
-                        </div>
+                <form action="insertarAL.php" method="POST">
+                    <input class="form-control mb-3" type="text" name="id_prestamo" placeholder="Id del prestamo" required>
+                    <input class="form-control mb-3" type="text" name="id_herramienta" placeholder="Clave de la herramienta" required>
+                    <input class="form-control mb-3" type="text" name="nombreHerramienta" placeholder="Nombre de la herramienta" required>
+                    <input class="form-control mb-3" type="number" name="cantidad" placeholder="Cantidad a prestar" required>
+                    <input class="form-control mb-3" type="number" name="matricula" placeholder="Matricula del alumno" required>
+                    <input class="form-control mb-3" type="text" name="nombreAlumno" placeholder="Nombre del Alumno" required>
+                    <input class="form-control mb-3" type="text" name="semestre" placeholder="Semestre" required>
+                    <label>Selecciona una carrera</label>
+                    <select name="carrera">
+                        <option>ISC</option>
+                        <option>Civil</option>
+                        <option>Industrial</option>
+                        <option>Administración</option>
+                    </select>
+                    <br><br>
+                    <input class="form-control mb-3" type="number" name="celular" placeholder="Número del alumno" pattern="[0-9]{10}" required>
+                    <label>FECHA DE PRESTAMO</label>
+                    <input class="form-control mb-3" type="date" name="fechaEntrega">                         
+                    <input class="btn btn-primary" type="submit">
+                </form>
+            </div>
+            <div class="col-md-8">
+                <table class="table" >
+                    <thead class="table-success table-striped" >
+                        <tr>
+                            <th>Clave</th>
+                            <th>nombreHerramienta</th>
+                            <th>descripcion</th>
+                            <th>laboratorio</th>
+                            <th>cantidad</th>
+                            <th>Prestados</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while($row=mysqli_fetch_array($query)){     
+                        ?>
+                        <tr>
+                            <th><?php echo $row['clave']?></th>
+                            <th><?php echo $row['nombreHerramienta']?></th>
+                            <th><?php echo $row['descripcion']?></th>
+                            <th><?php echo $row['lab']?></th>
+                            <th><?php echo $row['cantidad']?></th>
+                            <th><?php echo $row['estado']?></th>
+                        </tr>
+                        <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
+    </main>
 
-    </div>
+    <form class="unidad" action="prestamomain.php">  
+        <button class="boton" >Regresar</button>
     </form>
-        <div class="divi" >
-    <form action="prestamomain.php">  
-        <button class="contenedorB" >Regresar</button>
-        </form>
-        
-    </div>
-  </body>
-  </html>
+</body>
+</html>
--- prestamosM2.php
+++ prestamosM2.php
@@ -21,11 +21,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <title>Inventario</title>
+    <title>Prestamos Maestros</title>
 </head>
 <body>
     <header>
-        <h1>PRESTAMOS</h1>
+        <h1>PRESTAMOS MAESTROS</h1>
     </header>
 
     <main class="container at-5">
